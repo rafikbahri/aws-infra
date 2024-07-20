1cb66d835b998c9396e6605d77991434221d101e
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-07-20 10:12:45 +0200

    arn

--- modules/aws-group/main.tf
+++ modules/aws-group/main.tf
@@ -1,3 +1,16 @@
 resource "aws_iam_group" "group" {
   name = var.group_name
 }
+
+variable "policy_arns" {
+  description = "List of IAM policy ARNs to attach to the group"
+  type        = list(string)
+  default     = []
+}
+
+
+resource "aws_iam_group_policy_attachment" "policy_attach" {
+  count      = length(var.policy_arns)
+  group      = aws_iam_group.group.name
+  policy_arn = var.policy_arns[count.index]
+}
