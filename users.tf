bee800fe7b564e37f6ac87d79df21d9f03817299
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-07-20 11:12:54 +0200

    aws gh access wip

--- users.tf
+++ users.tf
@@ -1,31 +1,14 @@
-module "github-actions-role" {
-  source    = "./modules/aws-role"
-  role_name = "github-actions-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  ]
-}
-
 module "github-actions-user" {
   source = "./modules/aws-user"
 
   username = "github-actions-user"
+  path     = "/system/"
   policy_arns = [
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
   ]
+  tags = {
+    type    = "service"
+    context = "ci"
+    tool    = "github-actions"
+  }
 }
