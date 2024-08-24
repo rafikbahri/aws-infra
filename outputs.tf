1cfa5a78f88a2a89dc4414aca35e86ef4687f18f
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-08-24 11:27:48 +0200

    output private ips and icmp draft

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
-output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = module.bastions.instance_id
-}
-
 output "public_ip" {
   description = "Public IP of the created EC2 instance"
   value       = module.bastions.public_ip
+}
+
+output "bastions_private_ips" {
+  value = module.bastions.private_ips
+}
+
+output "etcd_private_ips" {
+  value = module.etcd-cluster.private_ips
 }
 
 output "private_key" {
