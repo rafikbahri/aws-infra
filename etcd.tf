4fd0a6cbb1d12ed62e0eba920063668e142cc891
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-08-26 19:57:25 +0200

    etcd in public subnet + ssm etcd ok

--- etcd.tf
+++ etcd.tf
@@ -1,13 +1,14 @@
-module "etcd-private-subnet" {
-  source                 = "./modules/aws-subnet"
-  name                   = "etcd-private-subnet"
-  vpc_id                 = module.main-vpc.vpc_id
-  availability_zone      = "eu-west-3a"
-  cidr_block             = var.etcd_subnet_cidr
-  default_route_table_id = module.main-vpc.route_table_id
-  has_internet_access    = true
+module "etcd-subnet" {
+  source                  = "./modules/aws-subnet"
+  name                    = "etcd-subnet"
+  vpc_id                  = module.main-vpc.vpc_id
+  availability_zone       = "eu-west-3a"
+  cidr_block              = var.etcd_subnet_cidr
+  map_public_ip_on_launch = true
+  default_route_table_id  = module.main-vpc.route_table_id
+  has_internet_access     = true
   tags = {
-    kind  = "private"
+    kind  = "public"
     group = "etcd"
   }
 }
@@ -19,10 +20,10 @@ module "etcd-cluster" {
   ami_id          = "ami-0546127e0cf2c6498"
   instance_type   = "t2.micro"
   vpc_id          = module.main-vpc.vpc_id
-  subnet_id       = module.etcd-private-subnet.subnet_id
+  subnet_id       = module.etcd-subnet.subnet_id
   private_ips     = [["192.168.16.11"], ["192.168.16.12"], ["192.168.16.13"]]
   create_key      = false
-  security_groups = [module.sg-admin.sg_id, module.sg-admin.sg_id, module.sg-ssm.sg_id]
+  security_groups = [module.sg-admin.sg_id]
   user_data_file  = ".config/cloudinit_user_data.yaml"
   tags = {
     component = "etcd"
