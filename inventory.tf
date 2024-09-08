a3d9729bb2b30be4c55499cf3755961a52515a74
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-09-08 13:19:10 +0200

    Users and etc hosts

--- inventory.tf
+++ inventory.tf
@@ -3,7 +3,10 @@ resource "local_file" "inventory" {
   file_permission = "0644"
   content         = <<EOT
 # Terraform managed
-[all]
+[${var.platform}:children]
+bastions
+etcd
+
 [bastions]
 %{for index in range(length(module.bastions.hostnames))~}
 ${element(module.bastions.hostnames, index)}
@@ -12,6 +15,28 @@ ${element(module.bastions.hostnames, index)}
 [etcd]
 %{for index in range(length(module.etcd-cluster.hostnames))~}
 ${element(module.etcd-cluster.hostnames, index)}
+%{endfor~}
+EOT
+}
+
+resource "local_file" "etchosts" {
+  filename        = "inventory/group_vars/${var.platform}/hosts.yml"
+  file_permission = "0644"
+  content         = <<EOT
+# Terraform managed
+---
+etc_hosts:
+%{for index in range(length(module.bastions.hostnames))~}
+  - ip: ${element(module.bastions.private_ips, index)}
+    names:
+      - ${element(module.bastions.hostnames, index)}
+      - ${element(module.bastions.hostnames, index)}.${var.platform}
+%{endfor~}
+%{for index in range(length(module.etcd-cluster.hostnames))~}
+  - ip: ${element(module.etcd-cluster.private_ips, index)}
+    names:
+      - ${element(module.etcd-cluster.hostnames, index)}
+      - ${element(module.etcd-cluster.hostnames, index)}.${var.platform}
 %{endfor~}
 EOT
 }
