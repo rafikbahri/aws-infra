d5b83ce90a2698eabf0e3d1b62ee66e994df128f
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-09-11 21:58:52 +0200

    wip install etcd + security groups ok

--- etcd-cluster.tf
+++ etcd-cluster.tf
@@ -12,6 +12,53 @@ module "etcd-subnet" {
   }
 }
 
+module "sg-etcd" {
+  source      = "./modules/aws-sg"
+  name        = "sg_etcd"
+  description = "Etcd member security group"
+  vpc_id      = module.main-vpc.vpc_id
+  ingress_rules = [
+    {
+      description = "Etcd listen to clients port."
+      from_port   = 2379
+      to_port     = 2379
+      protocol    = "tcp"
+      cidr_blocks = []
+      # Required attribues: https://stackoverflow.com/a/69080432/5684155
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description = "Etcd listen to peers port."
+      from_port   = 2380
+      to_port     = 2380
+      protocol    = "tcp"
+      cidr_blocks = []
+      # Required attribues: https://stackoverflow.com/a/69080432/5684155
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = true
+    }
+  ]
+  egress_rules = [
+    {
+      description      = "Allow all outbound traffic."
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = true
+    }
+  ]
+}
+
+
 module "etcd-cluster" {
   source          = "./modules/aws-node"
   server_count    = var.etcd_cluster_servers_count
@@ -22,7 +69,7 @@ module "etcd-cluster" {
   subnet_id       = module.etcd-subnet.subnet_id
   private_ips     = [["192.168.16.11"], ["192.168.16.12"], ["192.168.16.13"]]
   create_key      = false
-  security_groups = [module.sg-admin.sg_id]
+  security_groups = [module.sg-admin.sg_id, module.sg-etcd.sg_id]
   user_data_file  = ".config/cloudinit_user_data.yaml"
   tags = {
     component = "etcd"
