52d9db8f1e35d3835a11207569d8503764c44844
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-09-15 14:39:00 +0200

    Cleanup modules folder

--- modules/aws-role/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "role_arn" {
-  value       = aws_iam_role.role.arn
-  description = "ARN of the created IAM role"
-}
-
-output "role_name" {
-  value       = aws_iam_role.role.name
-  description = "Name of the created IAM role"
-}
