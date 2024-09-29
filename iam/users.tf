2e408bdecaa676b84f54d4504c200992bdf002ba
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-09-29 14:29:24 +0200

    Describe AZ perms for github-actions user (#38)

--- iam/users.tf
+++ iam/users.tf
@@ -1,11 +1,29 @@
-module "github-actions-user" {
-  source = "./modules/aws-user"
+# Mandatory for GH Actions terraform plan
+resource "aws_iam_policy" "describe_az_policy" {
+  name        = "EC2DescribeAvailabilityZonesPolicy"
+  path        = "/"
+  description = "IAM policy for describing EC2 Availability Zones"
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ec2:DescribeAvailabilityZones"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+module "github-actions-user" {
+  source   = "./modules/aws-user"
   username = "github-actions"
   path     = "/system/"
   policy_arns = [
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+    "arn:aws:iam::aws:policy/IAMReadOnlyAccess",
+    aws_iam_policy.describe_az_policy.arn
   ]
   tags = {
     type    = "service"
@@ -15,8 +33,7 @@ module "github-actions-user" {
 }
 
 resource "local_sensitive_file" "github-actions-credentials" {
-  filename = pathexpand("~/.config/aws-github-actions")
-
+  filename        = pathexpand("~/.config/aws-github-actions")
   content         = <<-EOT
           export GH_AWS_ACCESS_KEY_ID=${module.github-actions-user.access_key_id}
           export GH_AWS_SECRET_ACCESS_KEY=${module.github-actions-user.secret_access_key}
